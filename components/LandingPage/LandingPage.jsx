2f019c04fa32d16815b704b1342e34bad41d72e3
Muhindo-Galien <muhindogalien512@gmail.com>
2022-05-31 02:55:34 -0700

    Yokaa initialized

---
+++ components/LandingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../modules/Header'
+
+const LandingPage = () => {
+  return (
+    <div>
+        <Header/>
+    </div>
+  )
+}
+
+export default LandingPage
