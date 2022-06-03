a9fb515f631679c6b22ec52caa0fdb523c1ef2d2
Muhindo Galien <74584848+Muhindo-Galien@users.noreply.github.com>
2022-06-03 13:48:31 -0700

    Revert "Ft nav bars"

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,7 @@
-import "../styles/globals.css";
-import { RecoilRoot } from "recoil";
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
-  );
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,10 @@
-const defaultTailwindColorsColors = require("tailwindcss/colors");
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      ...defaultTailwindColorsColors,
-      primary: "#d97706",
-    },
-    screens: {
-      mobile: { max: "1024px" },
-    },
-    extend: {
-      transitionProperty: {
-        width: "width",
-      },
-      fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
-        Mulish: ["Mulish", "sans-serif"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
