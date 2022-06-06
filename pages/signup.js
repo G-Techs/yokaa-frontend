f86aef3cfafd141e97f61d5c6f8d6e385ed5ec15
Jonath-z <jonathanzihindula95@gmail.com>
2022-06-06 19:38:13 +0200

    auth pages added

---
+++ pages/signup.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SignupPage from "../components/SignupPage";
+
+const Signup = () => {
+  return (
+    <div className="auth h-screen">
+      <SignupPage />
+    </div>
+  );
+};
+
+export default Signup;
