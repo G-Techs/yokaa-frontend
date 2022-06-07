dfacb21b099e2333ef6295364307ee3edea7a800
Muhindo-Galien <muhindogalien512@gmail.com>
2022-06-07 09:11:28 -0700

    top songs added

---
+++ components/dummy_data/topSongsData.js
@@ -0,0 +1,44 @@
+export const topSongsData = [
+    {
+        id:1,
+        img:"https://templates.iqonic.design/muzik/html/images/dashboard/feature-album/01.png",
+        title:"sabali",
+        artist:"Damian Marley"
+    },
+    {
+        id:2,
+        img:"https://templates.iqonic.design/muzik/html/images/dashboard/popular-hindi-song/02.png",
+        title:"Holly Mountain",
+        artist:"Bob Marley"
+    },
+    {
+        id:3,
+        img:"https://templates.iqonic.design/muzik/html/images/dashboard/popular-hindi-song/06.png",
+        title:"In My pocket",
+        artist:"Duro lite"
+    },
+    {
+        id:4,
+        img:"https://templates.iqonic.design/muzik/html/images/dashboard/feature-album/01.png",
+        title:"Call away",
+        artist:"Charlie puth"
+    },
+    {
+        id:5,
+        img:"https://templates.iqonic.design/muzik/html/images/dashboard/popular-hindi-song/08.png",
+        title:"Go",
+        artist:"Tekno"
+    },
+    {
+        id:6,
+        img:"https://templates.iqonic.design/muzik/html/images/dashboard/popular-hindi-song/11.png",
+        title:"New york",
+        artist:"Alicia keys"
+    },
+    {
+        id:7,
+        img:"https://templates.iqonic.design/muzik/html/images/dashboard/popular-hindi-song/12.png",
+        title:"Got It On Me",
+        artist:"Summer Walker"
+    },
+]
---
+++ components/modules/Card/TopSongCard.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { topSongsData } from '../../dummy_data/topSongsData';
+import { PlayCircleIcon } from "../__modules/Vectors";
+
+const TopSongCard = ({data}) => {
+    const [onImageHover, setOnImageHover] = useState(false);
+    const [taggedElement, SetTaggedElement] = useState(10000);
+    const hover = (e)=>{
+        SetTaggedElement(e._targetInst.key)     
+    }
+    const songHover = (id)=>{
+        if(taggedElement == id){
+            setOnImageHover(true)
+        }
+    }
+    
+
+
+  return (
+    <div className="py-4">
+        <div className="flex flex-wrap -ml-4  -mr-4">
+            {data.map((song)=>{
+                const {id,title,img,artist} = song;
+                return(
+                    <div key={id} className="w-full md:w-1/3  lg:w-1/6 " onMouseEnter={hover} onMouseLeave={()=>setOnImageHover(false)} onMouseOver={()=>songHover(id)}>
+                        <div className="m-4 border border-red-200 rounded-bl-2xl rounded-tr-2xl h-50" >
+                            <div className="relative flex flex-col justify-center items-center">
+                                <img src={img} alt={artist} className="rounded-bl-2xl rounded-tr-2xl h-full md:h-32 w-full object-cover"/>
+                                <div className={`${onImageHover && id == taggedElement?'absolute':'hidden'}`} 
+>
+                                    <PlayCircleIcon className="text-4xl text-white cursor-pointer hover:bg-blend-darken"/>
+                                </div>
+                            </div>
+
+                           <div className="flex flex-col justify-center items-center p-4">
+                                <h1 className="text-gray-800   font-semibold capitalize">{title}</h1>
+                                <h2 className="text-gray-400 tx-md font-medium  capitalize">{artist}</h2>
+                           </div>
+                        </div>
+
+                    </div>
+                )
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default TopSongCard
