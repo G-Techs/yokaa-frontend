ff9298bafd65879b73d1ab9fa4d2ce79053b9918
Muhindo-Galien <muhindogalien512@gmail.com>
2022-06-07 09:11:28 -0700

    top songs added

--- components/LandingPage/LandingPage.jsx
+++ components/LandingPage/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { TopNav, LeftNav } from "../modules/NavBar";
 import { useRecoilValue } from "recoil";
 import { leftNavPinedAtom } from "../lib/atoms";
+import TopSongs from "../modules/TopSongs/TopSongs";
 
 const LandingPage = () => {
   const isLeftNavPined = useRecoilValue(leftNavPinedAtom);
@@ -14,6 +15,7 @@ const LandingPage = () => {
         } mobile:ml-5`}
       >
         <TopNav />
+        <TopSongs/>
       </div>
       <LeftNav />
     </>
