700e0c58152ff776b72f6a2e1c53d8127934e728
Jonath-z <jonathanzihindula95@gmail.com>
2022-06-16 19:47:38 +0200

    animated equalizer added

--- components/modules/HotSongs/index.jsx
+++ components/modules/HotSongs/index.jsx
@@ -6,7 +6,7 @@ import HotSongsCard from "./HotSongsCard";
 const HotSongs = () => {
   const [data, setData] = useState(topSongsData.slice(0, 12));
   return (
-    <div className="flex mr-5 mobile:flex-col gap-8  mt-6">
+    <div className="flex mr-5 mobile:flex-col gap-8 mt-6 mb-24">
       <HotSongsCard data={data} />
       <HotSongsByLocation data={data} location={"in DRC"} />
     </div>
