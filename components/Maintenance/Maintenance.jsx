d843310aba75d5ac0a68ce456e3c0b6570315328
Francois18 <79960710+Francois18@users.noreply.github.com>
2022-06-23 20:22:08 +0300

    Update Maintenance.jsx
    Color changed to Primary

--- components/Maintenance/Maintenance.jsx
+++ components/Maintenance/Maintenance.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { useRouter } from "next/router";
-import{ExclamationCircle,
+import {
+    ExclamationCircle,
     LayoutTopLine,
-    Clock} from '../modules/__modules/Vectors/index'
+    Clock
+} from '../modules/__modules/Vectors/index'
 
 const Maintenance = () => {
     const router = useRouter();
@@ -11,7 +13,7 @@ const Maintenance = () => {
         <div className='flex flex-wrap min-h-screen items-center justify-center '>
             <div className=' max-w-6xl p-4'>
                 <div className=' flex flex-col items-center justify-center text-center ' >
-                    <img src="https://templates.iqonic.design/muzik/html/images/error/02.png" alt="logo" className='sm:h-56 h-28  mb-6'/>
+                    <img src="https://templates.iqonic.design/muzik/html/images/error/02.png" alt="logo" className='sm:h-56 h-28  mb-6' />
                     <h1 className='font-extrabold text-4xl p-4'>We are Currently Performing Maintenance</h1>
                     <p className='font-lighter mb-4 p-2'>Please chack back sometime</p>
                 </div>
@@ -19,21 +21,27 @@ const Maintenance = () => {
 
                     <div className=' rounded-lg p-4 m-1 md:w-full shadow-xl lg:shadow-2xl'>
                         <div className='px-4 py-2 flex flex-col justify-center items-center'>
-                            < LayoutTopLine size={54} color='#FF5823'/>
+                            <span className='text-primary '>
+                                < LayoutTopLine size={54} />
+                            </span>
                             <h2 className='font-bold text-lg p-2'>Why is the Down</h2>
                             <p className='text-gray-600 font-light'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
                         </div>
                     </div>
                     <div className=' rounded-lg p-4 m-1 md:w-full shadow-xl lg:shadow-2xl'>
                         <div className='px-4 py-2 flex flex-col justify-center items-center'>
-                            <Clock size={54} color='#FF5823' />
+                            <span className='text-primary '>
+                                <Clock size={54} />
+                            </span>
                             <h2 className='font-bold text-lg p-2'>What is the Downtime?</h2>
                             <p className='text-gray-600 font-light'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
                         </div>
                     </div>
                     <div className=' rounded-lg p-4 m-1 md:w-full shadow-xl lg:shadow-2xl'>
                         <div className='px-4 py-2 flex flex-col justify-center  items-center '>
-                            <ExclamationCircle size={54} color='#FF5823' />
+                            <span className='text-primary'>
+                                <ExclamationCircle size={54} />
+                            </span>
                             <h2 className='font-bold text-lg p-2'>Do you need Support?</h2>
                             <p className='text-gray-600 font-light'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
                         </div>
