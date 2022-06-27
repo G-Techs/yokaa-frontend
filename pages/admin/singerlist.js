dc898f421c5871626af83ec9f0b6fbdd42275234
Muhindo-Galien <muhindogalien512@gmail.com>
2022-06-27 08:04:31 -0700

    singerlist init

---
+++ pages/admin/singerlist.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import SingerListPage from '../../components/SingerListPage'
+
+const singerlist = () => {
+  return (
+    <SingerListPage/>
+  )
+}
+
+export default singerlist
