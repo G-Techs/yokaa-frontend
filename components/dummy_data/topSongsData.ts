a03ef8cfb38e988c839d11f431f6cab6796c6e32
Muhindo-Galien <muhindogalien512@gmail.com>
2022-12-21 18:14:34 +0300

    TopSongs component migrated to ts

---
+++ components/dummy_data/topSongsData.ts
@@ -0,0 +1,86 @@
+export const topSongsData = [
+    {
+        id:1,
+        img:"https://templates.iqonic.design/muzik/html/images/dashboard/feature-album/01.png",
+        title:"sabali",
+        artist:"Damian Marley"
+    },
+    {
+        id:2,
+        img:"https://templates.iqonic.design/muzik/html/images/dashboard/popular-hindi-song/02.png",
+        title:"Holly Mountain",
+        artist:"Bob Marley"
+    },
+    {
+        id:3,
+        img:"https://templates.iqonic.design/muzik/html/images/dashboard/popular-hindi-song/06.png",
+        title:"In My pocket",
+        artist:"Duro lite"
+    },
+    {
+        id:4,
+        img:"https://templates.iqonic.design/muzik/html/images/dashboard/feature-album/01.png",
+        title:"Call away",
+        artist:"Charlie puth"
+    },
+    {
+        id:5,
+        img:"https://templates.iqonic.design/muzik/html/images/dashboard/popular-hindi-song/08.png",
+        title:"Go",
+        artist:"Tekno"
+    },
+    {
+        id:6,
+        img:"https://templates.iqonic.design/muzik/html/images/dashboard/popular-hindi-song/11.png",
+        title:"New york",
+        artist:"Alicia keys"
+    },
+    {
+        id:7,
+        img:"https://templates.iqonic.design/muzik/html/images/dashboard/popular-hindi-song/12.png",
+        title:"Got It On Me",
+        artist:"Summer Walker"
+    },
+    {
+        id:8,
+        img:"https://templates.iqonic.design/muzik/html/images/dashboard/feature-album/01.png",
+        title:"sabali",
+        artist:"Damian Marley"
+    },
+    {
+        id:9,
+        img:"https://templates.iqonic.design/muzik/html/images/dashboard/popular-hindi-song/02.png",
+        title:"Holly Mountain",
+        artist:"Bob Marley"
+    },
+    {
+        id:10,
+        img:"https://templates.iqonic.design/muzik/html/images/dashboard/popular-hindi-song/06.png",
+        title:"In My pocket",
+        artist:"Duro lite"
+    },
+    {
+        id:11,
+        img:"https://templates.iqonic.design/muzik/html/images/dashboard/feature-album/01.png",
+        title:"Call away",
+        artist:"Charlie puth"
+    },
+    {
+        id:12,
+        img:"https://templates.iqonic.design/muzik/html/images/dashboard/popular-hindi-song/08.png",
+        title:"Go",
+        artist:"Tekno"
+    },
+    {
+        id:13,
+        img:"https://templates.iqonic.design/muzik/html/images/dashboard/popular-hindi-song/11.png",
+        title:"New york",
+        artist:"Alicia keys"
+    },
+    {
+        id:14,
+        img:"https://templates.iqonic.design/muzik/html/images/dashboard/popular-hindi-song/12.png",
+        title:"Got It On Me",
+        artist:"Summer Walker"
+    },
+]
