023f977cffceab04cfa3e8771bf012ed8b25751e
Muhindo-Galien <muhindogalien512@gmail.com>
2022-12-26 16:13:24 +0300

    last changes made

--- components/AddNewSongPage/index.tsx
+++ components/AddNewSongPage/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { useState } from "react";
-import AddNewSinger from "../modules/AddNewsinger";
+import AddNewSinger from "../modules/AddNewSinger";
+
 import PageCard from "../modules/__modules__/Card/PageCard";
 
 const AddNewSongPage = () => {
   const [isTopNav, setIsTopNav] = useState(false);
   return (
     <PageCard setIsTopNav={setIsTopNav}>
-      <AddNewSinger isTopNav={isTopNav} />
+      <AddNewSinger />
     </PageCard>
   );
 };
