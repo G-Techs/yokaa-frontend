a3d0c562319503377dfedb335e5ad450da001876
Jonath-z <jonathanzihindula95@gmail.com>
2022-12-27 04:06:51 -0500

    feat: migrate AddCategory page and its all dependencies

---
+++ components/AddCategoryPage/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState } from "react";
+import AddCategory from "../modules/AddCategory";
+import PageCard from "../modules/__modules__/Card/PageCard";
+
+const AddCategoryPage = () => {
+  const [isTopNav, setIsTopNav] = useState(false);
+  
+  return (
+    <PageCard setIsTopNav={setIsTopNav}>
+      <AddCategory  />
+    </PageCard>
+  );
+};
+export default AddCategoryPage;
