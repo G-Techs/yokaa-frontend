cf39f8a7d2aa3e4551bbd521814fb4a037776cda
Muhindo-Galien <muhindogalien512@gmail.com>
2022-12-28 16:20:10 +0300

    display Signup page

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,14 +1,12 @@
 import React from "react";
-// import SignupPage from "../components/Auth/SignupPage";
+import SignupPage from "../components/Auth/SignupPage";
 
 const Signup = () => {
-  // return (
-  //   <div className="auth h-screen">
-  //     <SignupPage />
-  //   </div>
-  // );
-
-  return <></>;
+  return (
+    <div className="auth h-screen">
+      <SignupPage />
+    </div>
+  );
 };
 
 export default Signup;
