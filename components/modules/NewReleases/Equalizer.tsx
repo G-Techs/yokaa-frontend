c2be554f74e9b69e0f07b9aac9417df3d5b6cb2c
Jonath-z <jonathanzihindula95@gmail.com>
2022-12-29 12:44:27 +0200

    fix: equalizer responsive on mobile

--- components/modules/NewReleases/Equalizer.tsx
+++ components/modules/NewReleases/Equalizer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Equalizer = () => {
   return (
-    <div className="gap-1 relative h-12 w-[18.75rem]">
+    <div className="gap-1 relative h-12 w-[6rem] lg:w-[18.75rem]">
       <span className="bg-white w-4 h-full rounded-sm animate-equalizerBarOne absolute bottom-0" />
       <span className="bg-white w-4 h-full rounded-sm animate-equalizerBarTwo absolute ml-5 bottom-0" />
       <span className="bg-white w-4 h-full rounded-sm animate-equalizerBarThree absolute ml-10 bottom-0" />
